1357a20dfbd4e180d75129f383921f2749a50cc3
joshuamalmberg <jmalmber@sfu.ca>
2020-12-11 04:27:38 -0800

    Added handling of repeated timestamps in dataframe.

--- remove_discontinuities.py
+++ remove_discontinuities.py
@@ -9,22 +9,42 @@ from os.path import isfile, join, splitext
 
 min_duration = 5
 
-def est_sampling_frequency(df):
+def agg_repeated_timestamps(df):
+    grouped = df.groupby("time").mean()
+    return grouped.reset_index()
 
-    return freq
+def remove_motionless_windows(df, sampling_freq):
+    window_duration = 0.5   #if the sensor records minimal acceleration for a duration greater than this, then reject
+    window_size = int(window_duration*sampling_freq)
+    acc_magn = df["ax"]**2 + df["ay"]**2 + df["az"]**2
+    acc_magn = pd.Series(np.sqrt(acc_magn))
 
-def split_at_discountinuities(extensionless_filename, in_directory, out_directory):
+    stddev = acc_magn.std()
+    print(stddev)
+
+    windowed_avg_min = acc_magn.rolling(window = window_size).min()
+    return
+    
+    
+
+    # print(np.mean(acc_magn), np.max(acc_magn), np.median(acc_magn), np.percentile(acc_magn, 90))
+
+    return
+def clean_split(extensionless_filename, in_directory, out_directory):
     filepath = join(in_directory, extensionless_filename+".csv")
     df = pd.read_csv(filepath)
+    df = agg_repeated_timestamps(df)
     prev_obsv = df["time"].shift(periods=1, fill_value=0)
     diff = df["time"] - prev_obsv
-    z_abs = zscore(diff)
-    discontinuities = pd.Series(z_abs > 3)
+    # sampling_freq_est = 1/diff.median()
+    # remove_motionless_windows(df, sampling_freq_est)
+    z = zscore(diff)
+    discontinuities = pd.Series(z > 5)
     # print(discontinuities)
     df["cont_group"] = discontinuities.cumsum()
     # print(df)
 
-    # this for loop is used to split the dataframe up into pieces, cannot be done strictly with pandas
+    # this for loop is used to split the dataframe up into pieces, cannot be done strictly with pandas. safe since there are relatively few continuity group in each dataframe. each continuity group generally contains many 100s-1000s of rows
     for i in range(0, df["cont_group"].max()+1):
         sub_df = df[df["cont_group"] == i]
         sub_df = sub_df.drop(labels="cont_group", axis="columns")
@@ -40,12 +60,14 @@ def split_at_discountinuities(extensionless_filename, in_directory, out_director
             sub_df.to_csv(dest, index=False)
     return
 
+def filter()
+
 def main(in_dir, out_dir):
     files = [f for f in listdir(in_dir) if isfile(join(in_dir, f))]
     for file in files:
         filename, ext = splitext(file)
         if(ext == ".csv"):
-            split_at_discountinuities(filename, in_dir, out_dir)
+            clean_split(filename, in_dir, out_dir)
             print('\033[92m'+"Processed "+file+'\u001b[0m')
         else:
             print('\033[91m'+file+" rejected"+'\u001b[0m')
